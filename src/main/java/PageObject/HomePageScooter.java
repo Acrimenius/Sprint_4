527a47ff3195a45c24a4928d48422e6fc17148ae
Zviadi Ioseliani <knightoftime@yandex.ru>
2022-10-27 19:39:54 +0300

    add final project Sprint_4

---
+++ src/main/java/PageObject/HomePageScooter.java
@@ -0,0 +1,137 @@
+package PageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HomePageScooter {
+
+    private WebDriver driver;
+
+    private By subHeader = By.className("Home_SubHeader__zwi_E");
+    //локаторы для кнопок с вопросами
+    private By accordionButton_0 = By.id("accordion__heading-0");
+    private By accordionButton_1 = By.id("accordion__heading-1");
+    private By accordionButton_2 = By.id("accordion__heading-2");
+    private By accordionButton_3 = By.id("accordion__heading-3");
+    private By accordionButton_4 = By.id("accordion__heading-4");
+    private By accordionButton_5 = By.id("accordion__heading-5");
+    private By accordionButton_6 = By.id("accordion__heading-6");
+    private By accordionButton_7 = By.id("accordion__heading-7");
+
+    //локаторы текстов под кнопками с вопросами
+    private By accordionPanel_0 = By.id("accordion__panel-0");
+
+    private By accordionPanel_1 = By.id("accordion__panel-1");
+
+    private By accordionPanel_2 = By.id("accordion__panel-2");
+
+    private By accordionPanel_3 = By.id("accordion__panel-3");
+
+    private By accordionPanel_4 = By.id("accordion__panel-4");
+
+    private By accordionPanel_5 = By.id("accordion__panel-5");
+
+    private By accordionPanel_6 = By.id("accordion__panel-6");
+
+    private By accordionPanel_7 = By.id("accordion__panel-7");
+
+    public HomePageScooter(WebDriver driver) {
+
+        this.driver = driver;
+    }
+
+    //метод для прокрутки страницы
+    public void scrollPageDown() {
+        WebElement element = driver.findElement(By.id("accordion__heading-7"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    //методы для нажатия на стрелочку
+    public void clickOnFirstButtonWithQuestion(){
+
+        driver.findElement(accordionButton_0).click();
+
+    }
+
+
+    public void clickOnSecondButtonWithQuestion(){
+
+        driver.findElement(accordionButton_1).click();
+
+    }
+
+    public void clickOnThirdButtonWithQuestion(){
+
+        driver.findElement(accordionButton_2).click();
+
+    }
+
+    public void clickOnFourthButtonWithQuestion(){
+
+        driver.findElement(accordionButton_3).click();
+
+    }
+
+    public void clickOnFivethButtonWithQuestion(){
+
+        driver.findElement(accordionButton_4).click();
+
+    }
+
+    public void clickOnSixthButtonWithQuestion(){
+
+        driver.findElement(accordionButton_5).click();
+
+    }
+
+    public void clickOnSeventhButtonWithQuestion(){
+
+        driver.findElement(accordionButton_6).click();
+
+    }
+
+    public void clickOnEighthButtonWithQuestion(){
+
+        driver.findElement(accordionButton_7).click();
+
+    }
+
+    //Методы для получения текста под кнопками
+    public String textPannelFirst(){
+       return driver.findElement(By.id("accordion__panel-0")).getText();
+
+    }
+
+    public String textPannelSecond(){
+        return driver.findElement(By.id("accordion__panel-1")).getText();
+    }
+
+    public String textPannelThird(){
+        return driver.findElement(By.id("accordion__panel-2")).getText();
+    }
+
+    public String textPannelFourth(){
+        return driver.findElement(By.id("accordion__panel-3")).getText();
+    }
+
+    public String textPannelFiveth(){
+        return driver.findElement(By.id("accordion__panel-4")).getText();
+    }
+
+    public String textPannelSixth(){
+        return driver.findElement(By.id("accordion__panel-5")).getText();
+    }
+
+    public String textPannelSeventh(){
+        return driver.findElement(By.id("accordion__panel-6")).getText();
+    }
+
+    public String textPannelEighth(){
+        return driver.findElement(By.id("accordion__panel-7")).getText();
+    }
+
+
+
+}
---
+++ src/test/java/clickOnArrowAndCheckText.java
@@ -0,0 +1,189 @@
+import PageObject.HomePageScooter;
+import org.hamcrest.MatcherAssert;
+import org.junit.After;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public class clickOnArrowAndCheckText {
+    private WebDriver driver;
+
+
+    //тесты для проверки текста после нажатия на стрелочки
+    @Test
+    public void checkQuestions_1(){
+        //драйвер для хрома
+        driver = new ChromeDriver();
+
+
+        //переход на страницу тестового приложения
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+
+        HomePageScooter objHomePage = new HomePageScooter(driver);
+        objHomePage.scrollPageDown();
+        objHomePage.clickOnFirstButtonWithQuestion();
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("accordion__panel-0")));
+        String actualQuestionText = objHomePage.textPannelFirst();
+        String expectedQuestionText = "Сутки — 400 рублей. Оплата курьеру — наличными или картой.";
+        MatcherAssert.assertThat(actualQuestionText, is(expectedQuestionText));
+
+    }
+
+    @Test
+    public void checkQuestions_2(){
+        //драйвер для хрома
+        driver = new ChromeDriver();
+
+
+        //переход на страницу тестового приложения
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+
+        HomePageScooter objHomePage = new HomePageScooter(driver);
+        objHomePage.scrollPageDown();
+        objHomePage.clickOnSecondButtonWithQuestion();
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("accordion__panel-1")));
+        String actualQuestionText = objHomePage.textPannelSecond();
+        String expectedQuestionText = "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.";
+        MatcherAssert.assertThat(actualQuestionText, is(expectedQuestionText));
+
+    }
+
+    @Test
+    public void checkQuestions_3(){
+        //драйвер для хрома
+        driver = new ChromeDriver();
+
+
+        //переход на страницу тестового приложения
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+
+        HomePageScooter objHomePage = new HomePageScooter(driver);
+        objHomePage.scrollPageDown();
+        objHomePage.clickOnThirdButtonWithQuestion();
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("accordion__panel-2")));
+        String actualQuestionText = objHomePage.textPannelThird();
+        String expectedQuestionText = "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.";
+        MatcherAssert.assertThat(actualQuestionText, is(expectedQuestionText));
+
+    }
+
+    @Test
+    public void checkQuestions_4(){
+        //драйвер для хрома
+        driver = new ChromeDriver();
+
+
+        //переход на страницу тестового приложения
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+
+        HomePageScooter objHomePage = new HomePageScooter(driver);
+        objHomePage.scrollPageDown();
+        objHomePage.clickOnFourthButtonWithQuestion();
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("accordion__panel-3")));
+        String actualQuestionText = objHomePage.textPannelFourth();
+        String expectedQuestionText = "Только начиная с завтрашнего дня. Но скоро станем расторопнее.";
+        MatcherAssert.assertThat(actualQuestionText, is(expectedQuestionText));
+
+    }
+
+    @Test
+    public void checkQuestions_5(){
+        //драйвер для хрома
+        driver = new ChromeDriver();
+
+
+        //переход на страницу тестового приложения
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+
+        HomePageScooter objHomePage = new HomePageScooter(driver);
+        objHomePage.scrollPageDown();
+        objHomePage.clickOnFivethButtonWithQuestion();
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("accordion__panel-4")));
+        String actualQuestionText = objHomePage.textPannelFiveth();
+        String expectedQuestionText = "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.";
+        MatcherAssert.assertThat(actualQuestionText, is(expectedQuestionText));
+
+    }
+
+    @Test
+    public void checkQuestions_6(){
+        //драйвер для хрома
+        driver = new ChromeDriver();
+
+
+        //переход на страницу тестового приложения
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+
+        HomePageScooter objHomePage = new HomePageScooter(driver);
+        objHomePage.scrollPageDown();
+        objHomePage.clickOnSixthButtonWithQuestion();
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("accordion__panel-5")));
+        String actualQuestionText = objHomePage.textPannelSixth();
+        String expectedQuestionText = "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.";
+        MatcherAssert.assertThat(actualQuestionText, is(expectedQuestionText));
+
+    }
+
+    @Test
+    public void checkQuestions_7(){
+        //драйвер для хрома
+        driver = new ChromeDriver();
+
+
+        //переход на страницу тестового приложения
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+
+        HomePageScooter objHomePage = new HomePageScooter(driver);
+        objHomePage.scrollPageDown();
+        objHomePage.clickOnSeventhButtonWithQuestion();
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("accordion__panel-6")));
+        String actualQuestionText = objHomePage.textPannelSeventh();
+        String expectedQuestionText = "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.";
+        MatcherAssert.assertThat(actualQuestionText, is(expectedQuestionText));
+
+    }
+
+    @Test
+    public void checkQuestions_8(){
+        //драйвер для хрома
+        driver = new ChromeDriver();
+
+
+        //переход на страницу тестового приложения
+        driver.get("https://qa-scooter.praktikum-services.ru/");
+
+        HomePageScooter objHomePage = new HomePageScooter(driver);
+        objHomePage.scrollPageDown();
+        objHomePage.clickOnEighthButtonWithQuestion();
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.id("accordion__panel-7")));
+        String actualQuestionText = objHomePage.textPannelEighth();
+        String expectedQuestionText = "Да, обязательно. Всем самокатов! И Москве, и Московской области.";
+        MatcherAssert.assertThat(actualQuestionText, is(expectedQuestionText));
+
+    }
+
+    @After
+    public void teardown() {
+        // Закрытие браузер
+        driver.quit();
+    }
+
+
+    }
+
